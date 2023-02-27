bf3cb618e9248b002ac28f4de2ff271d2473809e
mwaghorn2000 <m.waghorn2000@gmail.com>
2023-02-27 14:12:39 +1100

    added styling and updated calculator logic

--- index.js
+++ index.js
@@ -1,3 +1,75 @@
+let firstOperator = "";
+let secondOperator = "";
+let currentOperator = null;
+let reset = false;
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById("equal-button");
+const clearButton = document.getElementById("clear");
+const oldScreen = document.getElementById("old-screen");
+const currentScreen = document.getElementById("current-screen");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => appendNumber(button.textContent));
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => setOperator(button.textContent));
+});
+
+equalsButton.addEventListener("click", () => evaluate());
+
+clearButton.addEventListener("click", () => clear());
+
+function appendNumber(num) {
+  if (currentScreen.textContent === "0" || reset) {
+    resetScreen();
+  }
+  currentScreen.textContent += num;
+}
+
+function appendOperator(operator) {}
+
+function resetScreen() {
+  currentScreen.textContent = "";
+  reset = false;
+}
+
+function clear() {
+  currentScreen.textContent = "";
+  oldScreen.textContent = "";
+  firstOperator = "";
+  secondOperator = "";
+  currentOperator = null;
+}
+
+function setOperator(operator) {
+  if (currentOperator !== null) evaluate();
+  firstOperator = currentScreen.textContent;
+  currentOperator = operator;
+  oldScreen.textContent = `${firstOperator} ${currentOperator}`;
+  reset = true;
+}
+
+function evaluate() {
+  if (currentOperator === null || reset) return;
+  if (currentOperator === "/" && currentScreen === "0") {
+    alert("You cannot divide by 0!");
+    return;
+  }
+  secondOperator = currentScreen.textContent;
+  currentScreen.textContent = roundResult(
+    operate(currentOperator, firstOperator, secondOperator)
+  );
+  oldScreen.textContent = `${firstOperator} ${currentOperator} ${secondOperator} =`;
+  currentOperator = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -6,7 +78,7 @@ function subtract(num1, num2) {
   return num1 - num2;
 }
 
-function mutiply(num1, num2) {
+function multiply(num1, num2) {
   return num1 * num2;
 }
 
@@ -14,52 +86,20 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operate(operator, num1, num2) {
-  if (operator === "+") {
-    return add(num1, num2);
-  } else if (operator === "-") {
-    return subtract(num1, num2);
-  } else if (operator === "*") {
-    return mutiply(num1, num2);
-  } else if (operator === "/") {
-    return divide(num1, num2);
-  } else if (operator === "C") {
-    clear();
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "x":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
   }
-}
-let displayValue = "";
-let equation = [];
-let display = document.querySelector("p");
-let buttons = document.querySelectorAll("button");
-buttons.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    if (e.target.id === "operator") {
-      equation = [e.target.innerText, parseInt(displayValue)];
-      displayValue = "";
-      display.innerText = displayValue;
-      console.log(equation);
-    } else if (e.target.id === "equals") {
-      if (!equation) {
-        return;
-      } else {
-        equation.push(parseInt(display.innerText));
-        console.log(equation);
-        displayValue = operate(
-          equation[0],
-          parseInt(equation[1]),
-          parseInt(equation[2])
-        );
-        console.log(displayValue);
-        display.innerText = displayValue;
-      }
-    } else {
-      displayValue += e.target.innerText;
-      display.innerText = displayValue;
-    }
-  });
-});
-function clear() {
-  displayValue = "";
-  equation = [];
-  display.innerText = displayValue;
 }
